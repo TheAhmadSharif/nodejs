474e0d8d53b23d7b979d5ebdc03be16c103bd9d9
Ahmad Sharif <TheAhmadSharif@gmail.com>
2019-09-25 18:56:34 +0600

    EventEmitter

--- index.js
+++ index.js
@@ -1,21 +1,8 @@
-var fs = require('fs');
+var events = require('events');
+var eventEmitterInstance = new events.EventEmitter();
 
-fs.readdir(__dirname, function (err, files) {
-  console.log(files);
-});
-
-fs.readFile('data.txt', function(err, data) {
-    console.log(data.toString(), 'Data');
-  });
-
-
-
-fs.writeFile('data_1.txt', 'This is my text', function (err) {
-  if (err) throw err;
-  console.log('Replaced!');
-});
-
-
-// Synchronous read
-var data = fs.readFileSync('data.txt');
-console.log("Synchronous read: " + data.toString())
+var hello = function shout () {
+	console.log('Shout');
+}
+eventEmitterInstance.on('myName', hello);
+eventEmitterInstance.emit('myName');
