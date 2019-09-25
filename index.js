c876ee6d5b17730893ed5ffc44205b1e864e721b
Ahmad Sharif <TheAhmadSharif@gmail.com>
2019-09-25 19:20:52 +0600

    howToWriteCodeMiddleWare

--- index.js
+++ index.js
@@ -1,8 +1,24 @@
-var events = require('events');
-var eventEmitterInstance = new events.EventEmitter();
+var express = require('express');
+var app = express();
 
-var hello = function shout () {
-	console.log('Shout');
-}
-eventEmitterInstance.on('myName', hello);
-eventEmitterInstance.emit('myName');
+
+app.get('/',
+	function(req,res, next){
+		if(Math.random() < 0.23) {
+			return next();	
+		} 
+		res.send('red');
+},
+	function(req,res, next){
+		if(Math.random() < 0.1) {
+			return next();
+		}
+		res.send('green');
+},
+	function(req,res){
+		res.send('blue');
+	},
+)
+
+
+app.listen('3400');
