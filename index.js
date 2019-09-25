afcdaa996784cca9a3c61730614ff4b95f13c863
Ahmad Sharif <TheAhmadSharif@gmail.com>
2019-09-25 21:21:01 +0600

    File upload with express-fileupload

--- index.js
+++ index.js
@@ -1,24 +1,31 @@
 var express = require('express');
+const fileUpload = require('express-fileupload');
+
 var app = express();
+app.use(fileUpload());
+
+app.use(express.static(__dirname + '/public'));
 
 
-app.get('/',
-	function(req,res, next){
-		if(Math.random() < 0.23) {
-			return next();	
-		} 
-		res.send('red');
-},
-	function(req,res, next){
-		if(Math.random() < 0.1) {
-			return next();
-		}
-		res.send('green');
-},
-	function(req,res){
-		res.send('blue');
-	},
-)
+app.get('/', function(req, res) {
+    res.render('index.html');
+});
 
 
-app.listen('3400');
+app.post('/upload', function(req, res) {
+  let sampleFile = req.files.foo;
+
+  console.log(sampleFile);
+
+  sampleFile.mv('/home/ahmad/Desktop/' + '/' + sampleFile.name, function(err) {
+    if (err)
+      return res.status(500).send(err);
+
+    res.send('File uploaded!');
+  });
+});
+
+
+app.listen(3400, () => {
+	console.log('Server is running at port 3400');
+})
