356f37003b981589bdb8a30a498b4b67c68fc8e3
Ahmad Sharif <TheAhmadSharif@gmail.com>
2019-09-29 19:16:30 +0600

    Img put into mongdb as a base64

--- index.js
+++ index.js
@@ -1,31 +1,80 @@
 var express = require('express');
-const fileUpload = require('express-fileupload');
-
+var MongoClient = require('mongodb').MongoClient;
+var uri = "mongodb://localhost:27017/Img"
 var app = express();
-app.use(fileUpload());
-
-app.use(express.static(__dirname + '/public'));
+var fs = require('fs');
 
 
-app.get('/', function(req, res) {
-    res.render('index.html');
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true });
+
+client.connect(err => {
+		const collection = client.db("Img").collection("store");
+  		collection.find().toArray((error, doc) => {
+  			if (doc) {
+  				//console.log(doc);
+  			}
+  		})
+
+  		var file2 = __dirname + '/' + 'data.txt'
+		var file = __dirname + '/' + '1.jpg';
+
+		var imageAsBase64 = fs.readFileSync(file, 'base64');
+
+
+
+		var base64data = imageAsBase64.toString();
+
+
+			collection.insertOne({imgObject: base64data}, (er, res) => {
+	  			console.log(res);
+	  		})
+
+
 });
 
 
-app.post('/upload', function(req, res) {
-  let sampleFile = req.files.foo;
+app.get('/', (req, res)=> {
+	client.connect(err => {
+		const collection = client.db("Img").collection("store");
 
-  console.log(sampleFile);
-
-  sampleFile.mv('/home/ahmad/Desktop/' + '/' + sampleFile.name, function(err) {
-    if (err)
-      return res.status(500).send(err);
-
-    res.send('File uploaded!');
-  });
-});
+  		collection.find().toArray((error, doc) => {
+  			if (doc) {
+  				
+  					 var thumb = doc[0].imgObject.toString();
 
 
-app.listen(3400, () => {
-	console.log('Server is running at port 3400');
+  					 console.log(thumb);
+
+
+  					 var thumbSrc = `data:image/jpg;base64,${thumb} `;  
+
+  					 console.log(thumbSrc, '----------------------------');
+
+
+  			
+  				res.send("<img src='"+thumbSrc+"'>");
+
+				
+  			}
+  		})
+
+
+	});
+	
 })
+
+
+
+app.get('/test', (req, res, next) => {
+	res.render('index');
+});
+
+
+
+
+app.listen('3400', () => {
+	console.log('Server is running at port 3400');
+}) 
+
+
