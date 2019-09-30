848ac71cdc688d317b81bcefa707c231a451cc24
Ahmad Sharif <TheAhmadSharif@gmail.com>
2019-09-30 21:07:02 +0600

    GridFS | CreateDB, UploadFile and download

--- index.js
+++ index.js
@@ -1,12 +1,34 @@
+const fs = require('fs');
 const mongodb = require('mongodb');
 const uri = 'mongodb://localhost:27017';
 const client = new mongodb.MongoClient(uri);
 
-client.connect((err) => {
-	if (!err) {
-		console.log('connection created');
-	}
-	const newDB = client.db("YourNewDatabase");
-	newDB.createCollection("YourCreatedCollectionName");
 
-})
+client.connect((err)=> {
+	var db = client.db("gridFSdb");
+	var bucket = new mongodb.GridFSBucket(db);
+
+	/*fs.createReadStream('./6.jpg').
+		pipe(bucket.openUploadStream('asdasdlkm.jpg')).
+		on('error', (error) => {
+			console.log(error);
+		}).
+		on('finish', () => {
+			console.log('done');
+			process.exit(0);
+		})*/;
+
+	bucket.openDownloadStreamByName('asdasdlkm.jpg').
+	  pipe(fs.createWriteStream('./alskjdkd.jpg')).
+	  on('error', function(error) {
+	    assert.ifError(error);
+	  }).
+	  on('end', function() {
+	    console.log('done!');
+	    process.exit(0);
+	  });
+
+
+
+
+});
